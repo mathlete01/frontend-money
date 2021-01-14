8876bc9fea371d2f1fbfdee232038f631ddd1743
mathlete01 <msallin@gmail.com>
2021-01-13 21:40:04 -0800

    Make wizard form work

--- src/components/MasterForm.js
+++ src/components/MasterForm.js
@@ -6,64 +6,111 @@ import Step2 from "./Step2";
 import Step3 from "./Step3";
 import Step4 from "./Step4";
 
-class MasterForm extends Component {
+class MasterForm extends React.Component {
   constructor(props) {
     super(props);
-
-    // this._next = this._next.bind(this)
-    // this._prev = this._prev.bind(this)
-
     this.state = {
       currentStep: 1,
-      monthly_income: 3000,
-      monthly_bills: 1000,
+      email: "",
+      username: "",
+      password: "",
     };
-    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(event) {
+  handleChange = (event) => {
     const { name, value } = event.target;
     this.setState({
       [name]: value,
     });
-  }
+  };
 
   handleSubmit = (event) => {
     event.preventDefault();
-    const { monthly_income, monthly_bills } = this
-      .statealert(`Your registration detail: \n 
-    Monthly_Income: ${monthly_income} \n
-    Monthly_Bills: ${monthly_bills}`);
+    const { email, username, password } = this.state;
+    alert(`Your registration detail: \n 
+             Email: ${email} \n 
+             Username: ${username} \n
+             Password: ${password}`);
   };
+
+  _next = () => {
+    let currentStep = this.state.currentStep;
+    currentStep = currentStep >= 2 ? 3 : currentStep + 1;
+    this.setState({
+      currentStep: currentStep,
+    });
+  };
+
+  _prev = () => {
+    let currentStep = this.state.currentStep;
+    currentStep = currentStep <= 1 ? 1 : currentStep - 1;
+    this.setState({
+      currentStep: currentStep,
+    });
+  };
+
+  /*
+   * the functions for our button
+   */
+  previousButton() {
+    let currentStep = this.state.currentStep;
+    if (currentStep !== 1) {
+      return (
+        <button
+          className="btn btn-secondary"
+          type="button"
+          onClick={this._prev}
+        >
+          Previous
+        </button>
+      );
+    }
+    return null;
+  }
+
+  nextButton() {
+    let currentStep = this.state.currentStep;
+    if (currentStep < 3) {
+      return (
+        <button
+          className="btn btn-primary float-right"
+          type="button"
+          onClick={this._next}
+        >
+          Next
+        </button>
+      );
+    }
+    return null;
+  }
+
   render() {
     return (
       <React.Fragment>
-        {/* <h1>A Wizard Form!</h1> */}
+        <h1>React Wizard Form 🧙‍♂️</h1>
         <p>Step {this.state.currentStep} </p>
 
         <form onSubmit={this.handleSubmit}>
-          {/* Render the form steps and pass in the required props */}
+          {/* 
+          render the form steps and pass required props in
+        */}
           <Step1
             currentStep={this.state.currentStep}
             handleChange={this.handleChange}
-            monthly_income={this.state.monthly_income}
-            monthly_bills={this.state.monthly_bills}
+            email={this.state.email}
           />
           <Step2
             currentStep={this.state.currentStep}
             handleChange={this.handleChange}
-            four01k={this.state.four01k}
+            username={this.state.username}
           />
           <Step3
             currentStep={this.state.currentStep}
             handleChange={this.handleChange}
-            companyMatch={this.state.companyMatch}
+            password={this.state.password}
           />
-          <Step4
-            currentStep={this.state.currentStep}
-            handleChange={this.handleChange}
-            cc1_amount={this.state.cc1_amount}
-          />
+          {this.previousButton()}
+          {this.nextButton()}
         </form>
       </React.Fragment>
     );
@@ -71,3 +118,70 @@ class MasterForm extends Component {
 }
 
 export default MasterForm;
+
+// function Step1(props) {
+//   if (props.currentStep !== 1) {
+//     return null;
+//   }
+//   return (
+//     <div className="form-group">
+//       <label htmlFor="email">Email address</label>
+//       <input
+//         className="form-control"
+//         id="email"
+//         name="email"
+//         type="text"
+//         placeholder="Enter email"
+//         value={props.email}
+//         onChange={props.handleChange}
+//       />
+//     </div>
+//   );
+// }
+
+// function Step2(props) {
+//   if (props.currentStep !== 2) {
+//     return null;
+//   }
+//   return (
+//     <div className="form-group">
+//       <label htmlFor="username">Username</label>
+//       <input
+//         className="form-control"
+//         id="username"
+//         name="username"
+//         type="text"
+//         placeholder="Enter username"
+//         value={props.username}
+//         onChange={props.handleChange}
+//       />
+//     </div>
+//   );
+// }
+
+// function Step3(props) {
+//   if (props.currentStep !== 3) {
+//     return null;
+//   }
+//   return (
+//     <React.Fragment>
+//       <div className="form-group">
+//         <label htmlFor="password">Password</label>
+//         <input
+//           className="form-control"
+//           id="password"
+//           name="password"
+//           type="password"
+//           placeholder="Enter password"
+//           value={props.password}
+//           onChange={props.handleChange}
+//         />
+//       </div>
+//       <button className="btn btn-success btn-block">Sign up</button>
+//     </React.Fragment>
+//   );
+// }
+
+// ReactDOM.render(<MasterForm />, document.getElementById("root"));
+
+// export default MasterForm;
