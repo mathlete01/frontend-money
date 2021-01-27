f7ebe799e40ea8844af0e52d60c46493724cc10c
mathlete01 <msallin@gmail.com>
2021-01-26 21:59:55 -0800

    Style navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Switch, withRouter, NavLink } from "react-router-dom";
 // import Body from "./components/xBody";
-import NotFound from "./NotFound";
+// import NotFound from "./NotFound";
 import Credentials from "./components/Credentials";
 import { connect } from "react-redux";
 import { updateCurrentStep } from "./actions/stepActions";
-import { updateCurrentUser, setCurrentUser, getCurrentUser } from "./actions/userActions";
+import {
+  updateCurrentUser,
+  setCurrentUser,
+  getCurrentUser,
+} from "./actions/userActions";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -23,134 +27,93 @@ import Button from "react-bootstrap/Button";
 import StepContainer from "./components/StepContainer";
 import "./App.css";
 import Ladder from "./components/Ladder";
+import TopNav from "./components/TopNav"
 
 library.add(faSwimmingPool, faCoffee, faHandPointLeft, faChevronLeft);
 
 class App extends React.Component {
-  state = {
-    user: "",
-  };
+  // state = {
+  //   user: "",
+  // };
 
-  renderForm = (routerProps) => {
-    if (routerProps.location.pathname === "/login") {
-      return <Credentials name="Login Form" handleSubmit={this.handleLogin} />;
-    } else if (routerProps.location.pathname === "/signup") {
-      return (
-        <Credentials name="Signup Form" handleSubmit={this.handleSignup} />
-      );
-    }
-  };
+  // renderForm = (routerProps) => {
+  //   if (routerProps.location.pathname === "/login") {
+  //     return <Credentials name="Login Form" handleSubmit={this.handleLogin} />;
+  //   } else if (routerProps.location.pathname === "/signup") {
+  //     return (
+  //       <Credentials name="Signup Form" handleSubmit={this.handleSignup} />
+  //     );
+  //   }
+  // };
 
-  handleLogin = (credentialObj) => {
-    // debugger
-    console.log(`handleLogin: credentialObj = `, credentialObj);
-    this.handleAuthFetch(credentialObj, "http://localhost:3000/login");
-  };
+  // handleLogin = (credentialObj) => {
+  //   // debugger
+  //   console.log(`handleLogin: credentialObj = `, credentialObj);
+  //   this.handleAuthFetch(credentialObj, "http://localhost:3000/login");
+  // };
 
-  handleLogout = () => {
-    // console.log("Logout called");
-    localStorage.clear();
-    this.props.setCurrentUser({});
-    this.props.history.push("/");
-    window.location.reload(false);
-  };
+  // handleLogout = () => {
+  //   // console.log("Logout called");
+  //   localStorage.clear();
+  //   this.props.setCurrentUser({});
+  //   this.props.history.push("/");
+  //   window.location.reload(false);
+  // };
 
-  handleSignup = (credentialObj) => {
-    // debugger
-    console.log(`handleSignup: credentialObj = `, credentialObj);
-    if (Object.keys(this.props.currentUser).length === 0) {
-      this.handleAuthFetch(credentialObj, "http://localhost:3000/users");
-    } else {
-      this.props.updateCurrentUser(this.props.currentUser.id, {
-        username: credentialObj.username,
-        password: credentialObj.password,
-      }, this.props.currentStep);
-    }
-    this.props.history.push("/");
-  };
+  // handleSignup = (credentialObj) => {
+  //   // debugger
+  //   console.log(`handleSignup: credentialObj = `, credentialObj);
+  //   if (Object.keys(this.props.currentUser).length === 0) {
+  //     this.handleAuthFetch(credentialObj, "http://localhost:3000/users");
+  //   } else {
+  //     this.props.updateCurrentUser(
+  //       this.props.currentUser.id,
+  //       {
+  //         username: credentialObj.username,
+  //         password: credentialObj.password,
+  //       },
+  //       this.props.currentStep
+  //     );
+  //   }
+  //   this.props.history.push("/");
+  // };
 
-  handleAuthFetch = (credentialObj, endpoint) => {
-    console.log(`handleAuthFetch: credentialObj = `, credentialObj);
-    fetch(endpoint, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        user: {
-          username: credentialObj.username,
-          password: credentialObj.password,
-        },
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if(data.user){
-          localStorage.setItem("token", data.token)
-          this.props.setCurrentUser(data.user);
-          this.props.updateCurrentStep(data.user.current_step)
-        }else{
-          console.log(`data = `, data);
-        }
-      });
-  };
+  // handleAuthFetch = (credentialObj, endpoint) => {
+  //   console.log(`handleAuthFetch: credentialObj = `, credentialObj);
+  //   fetch(endpoint, {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       user: {
+  //         username: credentialObj.username,
+  //         password: credentialObj.password,
+  //       },
+  //     }),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       if (data.user) {
+  //         localStorage.setItem("token", data.token);
+  //         this.props.setCurrentUser(data.user);
+  //         this.props.updateCurrentStep(data.user.current_step);
+  //       } else {
+  //         console.log(`data = `, data);
+  //       }
+  //     });
+  // };
 
   render() {
     return (
-      <div>
-        <Navbar bg="white">
-          <Navbar.Brand>
-            {/* <FontAwesomeIcon icon="swimming-pool" /> */}
-            {/* <img
-              alt=""
-              src="./images/noun_Ladder_3196564.svg"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            /> */}{" "}
-            🪜 Climb the Ladder
-          </Navbar.Brand>
-          <Nav className="justify-content-end">
-            <Row>
-              <Col>
-            <Nav.Item>
-              {/* Nav.Link doesn't work */}
-              {/* <Nav.Link to="/signup">Sign Up</Nav.Link>  */}
-              <NavLink variant="secondary" size="sm" to="/signup">Sign Up</NavLink>
-              {/* Button refreshes the page */}
-              {/* <Button variant="secondary" size="sm" href="/signup">Sign Up</Button> */}
-            </Nav.Item>
-            </Col>
-            <Col>
-            <Nav.Item>
-              {/* <NavLink to="/login">Log in</NavLink> */}
-              <NavLink variant="secondary" size="sm" to="/login">
-                Log In
-              </NavLink>
-            </Nav.Item>
-            </Col>
-            <Col>
-            <Nav.Item>
-              <Button variant="secondary" size="sm" onClick={this.handleLogout}>
-                Log Out
-              </Button>
-            </Nav.Item>
-            </Col>
-            </Row>
-          </Nav>
-        </Navbar>
+      <Container>
+        <TopNav />
         <Container>
           <Row>
             {/* Left Margin */}
             <Col></Col>
             {/* Step Container */}
             <Col md={6} className="containerContainer">
-              <Switch>
-                <Route path="/" exact component={this.handleHome} />
-                <Route path="/login" exact component={this.renderForm} />
-                <Route path="/signup" exact component={this.renderForm} />
-                <Route component={NotFound} />
-              </Switch>
               <StepContainer className="h-100" />
             </Col>
             {/* Ladder */}
@@ -161,7 +124,7 @@ class App extends React.Component {
             <Col></Col>
           </Row>
         </Container>
-      </div>
+      </Container>
     );
   }
 }
@@ -177,5 +140,5 @@ export default connect(mapStateToProps, {
   updateCurrentStep,
   setCurrentUser,
   updateCurrentUser,
-  getCurrentUser
+  getCurrentUser,
 })(withRouter(App));
---
+++ src/components/TopNav.js
@@ -0,0 +1,168 @@
+import React from "react";
+import { Route, Switch, withRouter, Link, NavLink } from "react-router-dom";
+// import Body from "./components/xBody";
+import Credentials from "./Credentials";
+import { connect } from "react-redux";
+import { updateCurrentStep } from "./../actions/stepActions";
+import {
+  updateCurrentUser,
+  setCurrentUser,
+  getCurrentUser,
+} from "./../actions/userActions";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Navbar from "react-bootstrap/Navbar";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faSwimmingPool,
+  faCoffee,
+  faHandPointLeft,
+  faChevronLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Nav from "react-bootstrap/Nav";
+import Button from "react-bootstrap/Button";
+import NotFound from "./../NotFound";
+import { Form, FormControl, NavItem } from "react-bootstrap";
+
+library.add(faSwimmingPool, faCoffee, faHandPointLeft, faChevronLeft);
+
+class TopNav extends React.Component {
+  state = {
+    user: "",
+  };
+
+  renderForm = (routerProps) => {
+    if (routerProps.location.pathname === "/login") {
+      return <Credentials name="Login Form" handleSubmit={this.handleLogin} />;
+    } else if (routerProps.location.pathname === "/signup") {
+      return (
+        <Credentials name="Signup Form" handleSubmit={this.handleSignup} />
+      );
+    }
+  };
+
+  handleLogin = (credentialObj) => {
+    // debugger
+    console.log(`handleLogin: credentialObj = `, credentialObj);
+    this.handleAuthFetch(credentialObj, "http://localhost:3000/login");
+  };
+
+  handleLogout = () => {
+    // console.log("Logout called");
+    localStorage.clear();
+    this.props.setCurrentUser({});
+    this.props.history.push("/");
+    window.location.reload(false);
+  };
+
+  handleSignup = (credentialObj) => {
+    // debugger
+    console.log(`handleSignup: credentialObj = `, credentialObj);
+    if (Object.keys(this.props.currentUser).length === 0) {
+      this.handleAuthFetch(credentialObj, "http://localhost:3000/users");
+    } else {
+      this.props.updateCurrentUser(
+        this.props.currentUser.id,
+        {
+          username: credentialObj.username,
+          password: credentialObj.password,
+        },
+        this.props.currentStep
+      );
+    }
+    this.props.history.push("/");
+  };
+
+  handleAuthFetch = (credentialObj, endpoint) => {
+    console.log(`handleAuthFetch: credentialObj = `, credentialObj);
+    fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+          username: credentialObj.username,
+          password: credentialObj.password,
+        },
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.user) {
+          localStorage.setItem("token", data.token);
+          this.props.setCurrentUser(data.user);
+          this.props.updateCurrentStep(data.user.current_step);
+        } else {
+          console.log(`data = `, data);
+        }
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <Navbar>
+            <Navbar.Brand>Make Money Moves</Navbar.Brand>
+            {/* <Navbar.Toggle /> */}
+            <Form inline>
+                <Switch>
+                  <Route path="/" exact component={this.handleHome} />
+                  <Route path="/login" exact component={this.renderForm} />
+                  <Route path="/signup" exact component={this.renderForm} />
+                  <Route component={NotFound} />
+                </Switch>
+              </Form>
+            <Navbar.Collapse className="justify-content-end">
+            <Nav>
+                <NavItem href="/">
+                  <Nav.Link as={Link} to="/login">
+                    Log in
+                  </Nav.Link>
+                </NavItem>
+              </Nav>
+              <Nav>
+                <NavItem href="/">
+                  <Nav.Link as={Link} to="/signup">
+                    Sign up
+                  </Nav.Link>
+                </NavItem>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+        <div>
+          {/* <Navbar>
+            <Navbar.Collapse>
+              <Form inline>
+                <Switch>
+                  <Route path="/" exact component={this.handleHome} />
+                  <Route path="/login" exact component={this.renderForm} />
+                  <Route path="/signup" exact component={this.renderForm} />
+                  <Route component={NotFound} />
+                </Switch>
+              </Form>
+            </Navbar.Collapse>
+          </Navbar> */}
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    currentStep: state.stepReducer.currentStep,
+    currentUser: state.userReducer.currentUser,
+  };
+};
+
+export default connect(mapStateToProps, {
+  updateCurrentStep,
+  setCurrentUser,
+  updateCurrentUser,
+  getCurrentUser,
+})(withRouter(TopNav));
